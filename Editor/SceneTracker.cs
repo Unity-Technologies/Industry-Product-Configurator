fb04183a94c33205b8aa8dd07ff6bcac614beebd
unitydereklam <derek.lam@unity3d.com>
2024-05-03 14:13:07 +0100

    Clean up and update gitignore

--- Editor/SceneTracker.cs
+++ Editor/SceneTracker.cs
@@ -6,35 +6,8 @@ using UnityEngine;
 
 namespace IndustryCSE.Tool.ProductConfigurator.Editor
 {
-    //[InitializeOnLoad]
     public static class SceneTracker
     {
-        /*
-        private static void CheckForVariantSet()
-        {
-            var variantSets = Object.FindObjectsByType<VariantSetBase>(FindObjectsInactive.Include, FindObjectsSortMode.None);
-            foreach (var variantSet in variantSets)
-            {
-                if(variantSet.VariantSetAsset == null) continue;
-                if(variantSet.VariantBase == null || variantSet.VariantBase.Count == 0) continue;
-                foreach (var variantBase in variantSet.VariantBase)
-                {
-                    if(variantBase.conditionalVariants == null || variantBase.conditionalVariants.Count == 0) continue;
-                    List<ConditionalVariantData> toRemove = new List<ConditionalVariantData>();
-                    foreach (var baseConditionalVariant in variantBase.conditionalVariants)
-                    {
-                        if(variantSets.Any(x => x.VariantSetAsset == baseConditionalVariant.variantSetAsset)) continue;
-                        toRemove.Add(baseConditionalVariant);
-                    }
-                    foreach (var conditionalVariantData in toRemove)
-                    {
-                        Undo.RecordObject(variantSet, "Remove Conditional Variant Data");
-                        variantBase.conditionalVariants.Remove(conditionalVariantData);
-                    }
-                }
-            }
-        }*/
-        
         public static void CheckVariantSet(VariantSetBase variantSet)
         {
             var variantSets = Object.FindObjectsByType<VariantSetBase>(FindObjectsInactive.Include, FindObjectsSortMode.None).Where
